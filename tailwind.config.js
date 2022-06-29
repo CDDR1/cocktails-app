56220b26a099eb20a108344254c332e5bfb0950c
Carlos Duque <carlos.d.duque.r@gmail.com>
2022-06-28 21:56:02 -0400

    Adding custom colors to Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "primary": "#030027",
+                "secondary": "#4C1C00",
+                "accent": "#FFE74C",
+                "ghost-white": "#FAFAFF",
+                "old-grey": "#6B6570",
+            },
+        },
     },
     plugins: [],
 };
